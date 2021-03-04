493ec7e4555c2fe00294478d4d4da82765d89c42
loongEgg <loongEgg@users.noreply.github.com>
2021-03-04 23:02:43 +0800

    Signal moved to LoongEgg.Data

--- src/LoongEgg.Chart/contracts/ISignal.cs
+++ src/LoongEgg.Chart/contracts/ISignal.cs
@@ -3,23 +3,5 @@ using System.ComponentModel;
 
 namespace LoongEgg.Chart
 {
-    public interface ISignal
-    {
-        /// <summary>
-        /// 当前的数值
-        /// </summary>
-        double Value { get; set; }
-        /// <summary>
-        /// 标签
-        /// </summary>
-        [DefaultValue("LABEL")]
-        string Label { get; set; } 
-        /// <summary>
-        /// 单位
-        /// </summary>
-        [DefaultValue("-")]
-        string Unit { get; set; }
-
-        event EventHandler ValueChanged;
-    }
+    
 }
