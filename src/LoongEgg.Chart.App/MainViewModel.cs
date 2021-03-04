eb96fb7207a62f69e249093e5ef7f35dbc0962a6
LoongEgg <LoongEgg@163.com>
2021-03-04 16:34:03 +0800

    ListBox UniformGrid 和TimerChart组合工作基本正常

--- src/LoongEgg.Chart.App/MainViewModel.cs
+++ src/LoongEgg.Chart.App/MainViewModel.cs
@@ -20,11 +20,20 @@ namespace LoongEgg.Chart.App
             vm.SignalGroup_2 = new ObservableCollection<Signal>() { Signal.CosSignal, Signal.SquareSignal };
             vm.SignalGroup_3 = new ObservableCollection<Signal>() { Signal.SquareSignal, Signal.TriangleSignal };
 
+            vm.SignalGroups = new ObservableCollection<ObservableCollection<Signal>>
+            {
+                vm.SignalGroup_1,
+                vm.SignalGroup_2,
+                vm.SignalGroup_3
+            };
+
             return vm;
         }
 
         public ObservableCollection<Signal> SignalGroup_1 { get; set; }
         public ObservableCollection<Signal> SignalGroup_2 { get; set; }
         public ObservableCollection<Signal> SignalGroup_3 { get; set; }
+
+        public ObservableCollection<ObservableCollection<Signal>> SignalGroups { get; set; }
     }
 }
